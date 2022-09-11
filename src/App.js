d03302021bdf68d1b475395a148c78a2c722d8be
dakdos <zulfahmii051@gmail.com>
2022-09-11 19:42:59 +0700

    Belajar State

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, {Component} from "react";
+
+class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      name : '',
+    };
+  }
+
+  setName = (ev) => {
+    let name = ev.target.value
+    this.setState({
+      name : name
+    });
+  }
+  
+  render() {
+    return (
+      <div>
+        <h1>{this.state.name}</h1>
+        <input onChange={this.setName}/>
+      </div>
+    );
+  }
 }
 
 export default App;
