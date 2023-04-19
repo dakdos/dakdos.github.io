7506986ef81d1f5a4669ba9a648692df062b2516
dakdos <zulfahmii051@gmail.com>
2023-04-19 15:22:00 +0700

    change resume

--- src/components/portfolio/Portfolio.js
+++ src/components/portfolio/Portfolio.js
@@ -4,8 +4,8 @@ export default function Portfolio() {
     return (
         <Box>
             <Grid container display={'flex'} justifyContent={'center'}>
-                <object data="https://drive.google.com/file/d/1M5bL7PJlS6k2iMocGinJVbwuGqLgcd0b/preview" type="application/pdf" width="100%" height="950">
-                    <p>Alternative text - include a link <a href="https://drive.google.com/file/d/1M5bL7PJlS6k2iMocGinJVbwuGqLgcd0b/preview">to the PDF!</a></p>
+                <object data="https://drive.google.com/file/d/181jYwKJCpskRkJpArtMnJojWKux3KnY-/preview" type="application/pdf" width="100%" height="950">
+                    <p>Alternative text - include a link <a href="https://drive.google.com/file/d/181jYwKJCpskRkJpArtMnJojWKux3KnY-/preview">to the PDF!</a></p>
                 </object>
             </Grid>
         </Box>
